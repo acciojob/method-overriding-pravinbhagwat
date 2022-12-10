a29932ff1be81480c64332ee34c5004b2affb0ab
Pravin Bhagwat <pravinbhagwat150@gmail.com>
2022-12-10 18:37:40 +0530

    Task Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+    static class A{
+        void meth() {
+            System.out.println("Invoking method from class A");
+        }
+    }
+
+    static class B extends A {
+        @Override
+        void meth() {
+            System.out.println("Method is overridden in Extendend class B");
+        }
+    }
+    public static void main(String[] args) {
+        B b = new B();
+        b.meth();
+    }
 }
